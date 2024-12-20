226aa27f8f32deea65c9729cf07b06db13cb5cad
Cookie <mxgsoft@gmail.com>
2024-12-21 02:49:51 +0800

    update

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -6,5 +6,4 @@ envsubst < /etc/odoo/odoo.conf > /etc/odoo/odoo.conf.tmp
 mv /etc/odoo/odoo.conf.tmp /etc/odoo/odoo.conf
 
 # Start Odoo
-cd /usr/lib/python3/dist-packages
-exec python3 /usr/lib/python3/dist-packages/odoo/odoo-bin -c /etc/odoo/odoo.conf
+exec python3 /usr/bin/odoo -c /etc/odoo/odoo.conf
